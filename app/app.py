f635c4fcbd52de0790bfc6ccfdec62061bae842f
Kimutai <jankimsingoei@gmail.com>
2023-10-28 23:28:36 +0300

    add POST /restaurant_pizzas

--- app/app.py
+++ app/app.py
@@ -63,6 +63,41 @@ class PizzasResource(Resource):
         }for pizza in pizzas]
         response = make_response(jsonify(pizza_dict),200)
         return response
+class PizzaRestaurantResource(Resource):
+    def get(self):
+        pizza_restaurants = PizzaRestaurant.query.all()
+        pizza_restaurant_dict = [{
+            "price":round(pizza_restaurant.price,2),
+            "restaurant_id":pizza_restaurant.restaurant_id,
+            "pizza_id":pizza_restaurant.pizza_id
+        }for pizza_restaurant in pizza_restaurants]
+
+        response = make_response(jsonify(pizza_restaurant_dict),200)
+        return response
+    def post(self):
+        data = request.get_json()
+        price = data["price"]
+        pizza_id = data["pizza_id"]
+        restaurant_id = data["restaurant_id"]
+
+        if not price and not pizza_id and not restaurant_id:
+            return make_response(jsonify({"error":"Missing arguments"}), 400)
+        else:
+            pizza = Pizza.query.filter(Pizza.id == pizza_id).first()
+            restaurant = Restaurant.query.filter(Restaurant.id == restaurant_id).first()
+            if not pizza and not restaurant:
+                return make_response(jsonify({'errors': ['Invalid pizza or restaurant ID']}), 400)
+            else:
+                pizza_restaurant = PizzaRestaurant(price=price,restaurant=restaurant,pizza=pizza)
+                db.session.add(pizza_restaurant)
+                db.session.commit()
+            pizza_data = {
+                'id': pizza.id,
+                'name': pizza.name,
+                'ingredients': pizza.ingredients
+            }
+            return make_response(jsonify(pizza_data), 201 ) 
+api.add_resource(PizzaRestaurantResource,"/restaurant_pizzas")
 api.add_resource(PizzasResource,"/pizzas")
 api.add_resource(RestaurantResourceById,'/restaurants/<int:id>')
 api.add_resource(RestaurantResource,'/restaurants')
